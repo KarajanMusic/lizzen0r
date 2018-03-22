2091526ea7a5bd8779e0285d331499ea327fe699
Mike Christensen <mike_christensen@btinternet.com>
2018-03-22 19:43:48 +0100

    pretty

--- server/utils/jsondb.js
+++ server/utils/jsondb.js
@@ -5,7 +5,7 @@ const jsondb = {};
 
 jsondb.save = function(key, data) {
     db[key].push(data);
-    fs.writeFileSync('../data/db.json', JSON.stringify(db));
+    fs.writeFileSync('server/data/db.json', JSON.stringify(db, null, 2)); // eslint-disable-line no-sync
 };
 
 jsondb.get = function(key) {
