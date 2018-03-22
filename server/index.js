07996b298057d0450939e35eef62500f15f7521f
Mike Christensen <mike_christensen@btinternet.com>
2018-03-22 22:42:54 +0100

    fix

--- server/index.js
+++ server/index.js
@@ -39,10 +39,10 @@ logger.info('[SERVER] Initializing routes');
 require('./routes')(app, contracts);
 
 // Fallback everything else to the React application
-app.use(express.static(path.join(__dirname, '../client/dist')));
 app.get('googlef4c8fca73923a831.html', (req, res) => {
     res.sendFile(path.join(__dirname, '../data/googlef4c8fca73923a831.html'));
 });
+app.use(express.static(path.join(__dirname, '../client/dist')));
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
