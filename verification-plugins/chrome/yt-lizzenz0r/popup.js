348e9bfa5eea579f1c73c731c19855820d9487f1
Martin Aigner <martin@maigner.net>
2018-03-22 15:37:27 +0100

    demo features

--- verification-plugins/chrome/yt-lizzenz0r/popup.js
+++ verification-plugins/chrome/yt-lizzenz0r/popup.js
@@ -1,15 +1,29 @@
 console.log("popup.js loaded")
 
 let checkLicenseButton = document.getElementById('checkLicense');
-let anyLocalKeyField = document.getElementById('anyLocalKey')
+let anyLocalKeyField = document.getElementById('anyLocalKey');
+let resultField = document.getElementById('result');
+let licenseList = document.getElementById('licenseList')
 
-console.log(web3)
+var r = Math.random();
+if (r < 0.5) {
+  resultField.style.color = "#008000";
+  resultField.innerHTML = "License OK";
+  licenseList.innerHTML = "Issued by Red Bull MediaHouse"
+} else {
+  resultField.style.color = "#ff0000";
+  resultField.innerHTML = "No License found!"
+}
 
 chrome.storage.sync.get('anyLocalKey', function(data) {
-  anyLocalKeyField.innerHTML = data.anyLocalKey;
+  //anyLocalKeyField.innerHTML = data.anyLocalKey;
 });
 
+/*
 checkLicenseButton.onclick = function(element) {
+
+  var p = metamask.createDefaultProvider();
+  console.log(p);
 
   var ul = document.getElementById("licenseList");
   var li = document.createElement("li");
@@ -26,9 +40,44 @@ checkLicenseButton.onclick = function(element) {
     }
   );
 };
+*/
 
 chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
-    var url = tabs[0].url;
-    console.log(url)
-    document.getElementById("videoUrl").innerHTML = url
+    var id = tabs[0].url.split("=")[1];
+    console.log(id)
+    document.getElementById("videoUrl").innerHTML = id
 });
+
+
+
+
+/*
+const web3 = new Web3(new Web3.providers.WebsocketProvider('wss://mainnet.infura.io/ws'));
+
+const subscription = web3.eth.subscribe('newBlockHeaders', (error, blockHeader) => {
+  if (error) return console.error(error);
+  console.log('Successfully subscribed!', blockHeader);
+}).on('data', (blockHeader) => {
+  console.log('data: ', blockHeader);
+});
+
+*/
+
+function httpGetAsync(theUrl, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous
+    xmlHttp.send(null);
+}
+
+var url = "the url where we read license data"
+httpGetAsync(url, function(responseText) {
+  console.log(responseText);
+})
+
+
+/* */
