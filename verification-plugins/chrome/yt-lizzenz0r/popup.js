896e3daefa369b7115bdae6f5cf6bb34d35b9686
Martin Aigner <martin@maigner.net>
2018-03-22 11:36:06 +0100

    added some local storage and accessing page content

--- verification-plugins/chrome/yt-lizzenz0r/popup.js
+++ verification-plugins/chrome/yt-lizzenz0r/popup.js
@@ -1,6 +1,26 @@
+console.log("popup.js loaded")
+
 let changeColor = document.getElementById('changeColor');
+let anyLocalKeyField = document.getElementById('anyLocalKey')
 
 chrome.storage.sync.get('color', function(data) {
   changeColor.style.backgroundColor = data.color;
   changeColor.setAttribute('value', data.color);
 });
+
+chrome.storage.sync.get('anyLocalKey', function(data) {
+  anyLocalKeyField.innerHTML = data.anyLocalKey;
+});
+
+
+changeColor.onclick = function(element) {
+    let color = element.target.value;
+      chrome.tabs.executeScript(
+          null,
+          {code: 'document.body.style.backgroundColor = "' + color + '";'},
+          function(arg) {
+            console.log("callback")
+            console.log(arg)
+          }
+        );
+  };
