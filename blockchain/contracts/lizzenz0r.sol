9484265dc0f00eee34864135bf4afe73912034f8
Lukas Oberbichler <lukas@tribe.wtf>
2018-03-22 12:26:29 +0100

    Initial Blockchain Contract

---
+++ blockchain/contracts/lizzenz0r.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.4.21;
+
+contract SimpleStorage {
+    uint storedData;
+
+    function set(uint x) public {
+        storedData = x;
+    }
+
+    function get() public constant returns (uint) {
+        return storedData;
+    }
+
+
+
+    /**********
+     Standard kill() function to recover funds 
+     **********/
+    
+    function kill()
+    { 
+        if (msg.sender == creator)
+            suicide(creator);  // kills this contract and sends remaining funds back to creator
+    }
+}
